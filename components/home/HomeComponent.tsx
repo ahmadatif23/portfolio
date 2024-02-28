3d58e7c2468e8eee895618ad40c901a7177e5f2f
ahmadatif23 <ahmadatif23@gmail.com>
2024-02-28 17:06:00 +0800

    FIX: Escape symbol

--- components/home/HomeComponent.tsx
+++ components/home/HomeComponent.tsx
@@ -7,11 +7,11 @@ export default function HomeComponent() {
     <section className="bg-stripe flex w-full max-w-6xl flex-col-reverse items-center px-6 py-7 lg:flex-row xl:px-0">
       <div className="z-10 mt-10 text-center lg:mt-0 lg:w-[49%] lg:text-start">
         <p className="mb-8 text-xl font-medium md:text-2xl">
-          Hi, I'm <strong>Ahmad</strong>!
+          Hi, I&apos;m <strong>Ahmad</strong>!
         </p>
 
         <h1 className="lead whitespace-pre-wrap text-[6.8vw] font-extrabold !leading-[1.25] sm:text-[5vw] md:text-[4vw] lg:whitespace-normal lg:text-[3.2vw]">
-          I love creating things for webs. {"\n"}Here's where i share my{" "}
+          I love creating things for webs. {"\n"}Here&apos;s where i share my{" "}
           <Link href={"/"} className="text-outline">
             projects
           </Link>{" "}
